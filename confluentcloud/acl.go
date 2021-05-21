9f5174e3e6f6340bc606498ef79e4e1208f5c3b2
Alasdair Ellis <alasdair.ellis@imaginecurve.com>
2021-05-21 09:30:09 +0100

    feat: add delete acl functionality

---
+++ confluentcloud/acl.go
@@ -0,0 +1,56 @@
+package confluentcloud
+
+import (
+	"fmt"
+	"net/url"
+)
+
+type ACLCreateRequestW = []ACLCreateRequest
+type ACLCreateRequest struct {
+	Pattern *Pattern `json:"pattern"`
+	Entry   *Entry   `json:"entry"`
+}
+type Pattern struct {
+	ResourceType string `json:"resourceType"`
+	Name         string `json:"name"`
+	PatternType  string `json:"patternType"`
+}
+type Entry struct {
+	Principal      string `json:"principal"`
+	Operation      string `json:"operation"`
+	Host           string `json:"host"`
+	PermissionType string `json:"permissionType"`
+}
+
+type CreateACLResponse = []ACLCreateRequest
+
+
+func (c *Client) CreateACLs(apiEndpoint *url.URL, clusterID string, aclCreateRequestW *ACLCreateRequestW) (interface{}, error) {
+	token, err := c.GetKafkaClusterAccessToken()
+	if err != nil {
+		return nil, err
+	}
+	cc := NewKafkaClusterClient(apiEndpoint, clusterID, *token)
+
+	rel, err := url.Parse("acls")
+	if err != nil {
+		return nil, err
+	}
+
+	u := cc.BaseURL.ResolveReference(rel)
+
+	response, err := c.NewRequest().
+		SetAuthToken(*token).
+		SetBody(aclCreateRequestW).
+		SetResult(&CreateACLResponse{}).
+		Post(u.String())
+	if err != nil {
+		return nil, err
+	}
+	if response.IsError() {
+		return nil, fmt.Errorf("create_acls: %s", response.Body())
+	}
+
+	result := response.Result().(*CreateACLResponse)
+	return *result, nil
+}
